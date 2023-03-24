0c78e78f65efbbd76bb42ef561f489825c165c6b
MikaylaVen <mikaylavendryes@gmail.com>
2023-03-23 23:40:52 -0400

    changes

---
+++ lab8/lab8.js
@@ -0,0 +1,13 @@
+// Imports
+import express from "express"
+
+// Variables
+const app = express()
+const PORT = 3000
+
+// Listening on the PORT
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}.`)
+})
+
+// Add your routes below:
